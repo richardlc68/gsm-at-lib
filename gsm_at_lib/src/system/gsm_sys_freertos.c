67535604a8c357af5f83e2109fa0fc55b4697a70
Li Qiu Richard Liu <Richardl@paradox.com>
2020-05-07 16:12:42 -0400

    esp-at-lib already has freertos port, take it

--- gsm_at_lib/src/system/gsm_sys_freertos.c
+++ gsm_at_lib/src/system/gsm_sys_freertos.c
@@ -1,6 +1,6 @@
 /**
- * \file            gsm_sys_cmsis_os.c
- * \brief           System dependent functions for CMSIS-OS based operating system
+ * \file            gsm_sys_freertos_os.c
+ * \brief           System dependant functions
  */
 
 /*
@@ -26,22 +26,27 @@
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
  * OTHER DEALINGS IN THE SOFTWARE.
  *
- * This file is part of GSM-AT library.
- *
  * Author:          Tilen MAJERLE <tilen@majerle.eu>
  * Version:         $_version_$
  */
+
 #include "system/gsm_sys.h"
-#include "gsm_sys_port.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "semphr.h"
 
 #if !__DOXYGEN__
+/* Mutex ID for main protection */
+static SemaphoreHandle_t sys_mutex;
 
-static SemaphoreHandle_t  sys_mutex;
+typedef struct freertos_mbox {
+    void *d;
+} freertos_mbox;
 
 uint8_t
 gsm_sys_init(void) {
-    gsm_sys_mutex_create(&sys_mutex);
-    return pdTRUE;
+    sys_mutex = xSemaphoreCreateMutex();
+    return sys_mutex == NULL ? 0 : 1;
 }
 
 uint32_t
@@ -52,13 +57,13 @@ gsm_sys_now(void) {
 uint8_t
 gsm_sys_protect(void) {
     gsm_sys_mutex_lock(&sys_mutex);
-    return pdTRUE;
+    return 1;
 }
 
 uint8_t
 gsm_sys_unprotect(void) {
     gsm_sys_mutex_unlock(&sys_mutex);
-    return pdTRUE;
+    return 1;
 }
 
 uint8_t
@@ -70,18 +75,17 @@ gsm_sys_mutex_create(gsm_sys_mutex_t* p) {
 uint8_t
 gsm_sys_mutex_delete(gsm_sys_mutex_t* p) {
     vSemaphoreDelete(*p);
-    *p=NULL;
-    return pdTRUE;
+    return 1;
 }
 
 uint8_t
 gsm_sys_mutex_lock(gsm_sys_mutex_t* p) {
-    return xSemaphoreTakeRecursive(*p, osWaitForever) == pdTRUE;
+    return xSemaphoreTakeRecursive(*p, portMAX_DELAY) == pdPASS;
 }
 
 uint8_t
 gsm_sys_mutex_unlock(gsm_sys_mutex_t* p) {
-    return xSemaphoreGiveRecursive(*p) == pdTRUE;
+    return xSemaphoreGiveRecursive(*p) == pdPASS;
 }
 
 uint8_t
@@ -97,31 +101,30 @@ gsm_sys_mutex_invalid(gsm_sys_mutex_t* p) {
 
 uint8_t
 gsm_sys_sem_create(gsm_sys_sem_t* p, uint8_t cnt) {
-    if(cnt>0) {
-        *p = xSemaphoreCreateCounting(cnt,0);
+    *p = xSemaphoreCreateBinary();
+
+    if (*p != NULL && cnt) {
+        xSemaphoreGive(*p);
     }
-    else {
-        *p = xSemaphoreCreateBinary();
-    }
-    return pdTRUE;
+
+    return *p != NULL;
 }
 
 uint8_t
 gsm_sys_sem_delete(gsm_sys_sem_t* p) {
     vSemaphoreDelete(*p);
-    *p=NULL;
-    return pdTRUE;
+    return 1;
 }
 
 uint32_t
 gsm_sys_sem_wait(gsm_sys_sem_t* p, uint32_t timeout) {
-    if(pdTRUE  == xSemaphoreTake(*p,timeout)) return 0;
-    return GSM_SYS_TIMEOUT;
+    uint32_t t = xTaskGetTickCount();
+    return xSemaphoreTake(*p, !timeout ? portMAX_DELAY : timeout) == pdPASS ? (xTaskGetTickCount() - t) : GSM_SYS_TIMEOUT;
 }
 
 uint8_t
 gsm_sys_sem_release(gsm_sys_sem_t* p) {
-    return xSemaphoreGiveRecursive(*p) == pdTRUE;
+    return xSemaphoreGive(*p) == pdPASS;
 }
 
 uint8_t
@@ -137,37 +140,58 @@ gsm_sys_sem_invalid(gsm_sys_sem_t* p) {
 
 uint8_t
 gsm_sys_mbox_create(gsm_sys_mbox_t* b, size_t size) {
-    return (*b = xQueueCreate(size,sizeof(void *))) != NULL;
+    *b = xQueueCreate(size, sizeof(freertos_mbox));
+    return *b != NULL;
 }
 
 uint8_t
 gsm_sys_mbox_delete(gsm_sys_mbox_t* b) {
-    if (uxQueueMessagesWaiting(*b) > 0 ) {
+    if (uxQueueMessagesWaiting(*b)) {
         return 0;
     }
     vQueueDelete(*b);
-    *b=0;
-    return pdTRUE;
+    return 1;
 }
 
 uint32_t
 gsm_sys_mbox_put(gsm_sys_mbox_t* b, void* m) {
-    return xQueueSend(*b, &m, osWaitForever) == pdTRUE ? 0 : GSM_SYS_TIMEOUT;
+    freertos_mbox mb;
+    uint32_t t = xTaskGetTickCount();
+
+    mb.d = m;
+    xQueueSend(*b, &mb, portMAX_DELAY);
+    return xTaskGetTickCount()-t;
 }
 
 uint32_t
 gsm_sys_mbox_get(gsm_sys_mbox_t* b, void** m, uint32_t timeout) {
-    return xQueueReceive(*b, m, timeout) == pdTRUE ? 0 : GSM_SYS_TIMEOUT;;
+    freertos_mbox mb;
+    uint32_t t = xTaskGetTickCount();
+
+    if (xQueueReceive(*b, &mb, !timeout ? portMAX_DELAY : timeout)) {
+       *m = mb.d;
+       return xTaskGetTickCount()-t;
+    }
+    return GSM_SYS_TIMEOUT;
 }
 
 uint8_t
 gsm_sys_mbox_putnow(gsm_sys_mbox_t* b, void* m) {
-    return xQueueSend(*b, &m, 0) == pdTRUE;
+    freertos_mbox mb;
+
+    mb.d = m;
+    return xQueueSendFromISR(*b, &mb, 0) == pdPASS;
 }
 
 uint8_t
 gsm_sys_mbox_getnow(gsm_sys_mbox_t* b, void** m) {
-    return xQueueReceive(*b, m, 0) == pdTRUE;
+    freertos_mbox mb;
+
+    if (xQueueReceive(*b, &mb, 0)) {
+       *m = mb.d;
+       return 1;
+    }
+    return 0;
 }
 
 uint8_t
@@ -183,29 +207,18 @@ gsm_sys_mbox_invalid(gsm_sys_mbox_t* b) {
 
 uint8_t
 gsm_sys_thread_create(gsm_sys_thread_t* t, const char* name, gsm_sys_thread_fn thread_func, void* const arg, size_t stack_size, gsm_sys_thread_prio_t prio) {
-    gsm_sys_thread_t id;
-    if(xTaskCreate((TaskFunction_t)thread_func, 
-                   name, 
-                   stack_size > 0 ? stack_size : GSM_SYS_THREAD_SS,
-                   arg,
-                   prio,
-                   &id) != pdPASS ) return pdFALSE;
-    *t = id;
-    return pdTRUE;
+    return xTaskCreate(thread_func, name, stack_size, arg, prio, t) == pdPASS ? 1 : 0;
 }
 
 uint8_t
 gsm_sys_thread_terminate(gsm_sys_thread_t* t) {
-    if (t != NULL) {
-        vTaskDelete(*t);
-        *t=NULL;
-    }
+    vTaskDelete(*t);
     return 1;
 }
 
 uint8_t
 gsm_sys_thread_yield(void) {
-    //osThreadYield();
+    taskYIELD();
     return 1;
 }
 
